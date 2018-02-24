62f2f68b51d6b3be8a24c2524ab28733500212b3
krishnendu <krishnendu1985@gmail.com>
2018-02-24 06:51:24 +0530

    adding code to multiplication and subtraction file

--- Number_Test/sub_number.py
+++ Number_Test/sub_number.py
@@ -2,7 +2,7 @@
 
 #Author: Krishnendu
 
-print("This is s addition program of two or more number\n")
+print("This is s Subtraction program of two or more number\n")
 a1=int(input("Please enter the 1st number\t"))
 a2=int(input("Please enter the 2nd number\t"))
-print("Please find the addition of ", a1, "+",a2,"=",(a1+a2))
+print("Please find the Subtraction of ", a1, "-",a2,"=",(a1-a2))

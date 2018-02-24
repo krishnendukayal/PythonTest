387d677e496c8b89429b379efa30bf7fca5053a2
krishnendu <krishnendu1985@gmail.com>
2018-02-24 23:19:21 +0530

    Few changes

---
+++ Loop_Test/for_loop.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+
+print("This is a for loop program\n")
+n=int(input("Please enter the number you wan tpo iterate\t"))
+for i in range(n):
+    print(i)
+print("End of for loop")

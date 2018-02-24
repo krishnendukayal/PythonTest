3e5c9cd965c3c1536434cdc55eeb003e930876f1
krishnendu <krishnendu1985@gmail.com>
2018-02-24 22:40:20 +0530

    Few changes

---
+++ Number_Test/module_number.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+#Author: Krishnendu
+
+print("This is s modulas program of two or more number\n")
+a1=int(input("Please enter the 1st number\t"))
+a2=int(input("Please enter the 2nd number\t"))
+print("Please find the Multiplication of ", a1, "%",a2,"=",(a1%a2))

629e5eaf577defecd8dd75d58c595fa234270b12
krishnendu <krishnendu1985@gmail.com>
2018-02-26 09:26:56 +0530

    Few changes

---
+++ Loop_Test/star.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#Author: Krishnendu Kayal
+
+print("This is a printing star program")
+num=int(input("Please input the number you want to use for star print\t"))
+for i in range(num):
+    for j in range(i):
+        print("*", end=" ")
+    print("\n")

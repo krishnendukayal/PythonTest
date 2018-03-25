68ed57220ecce5794748e1948624a76fa3832ae2
krishnendu <krishnendu1985@gmail.com>
2018-03-25 18:09:58 +0530

    Worked on module, package etc and created multiple file

--- Module_function/module1.py
+++ Module_function/module1.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 
+print("This will run all the time")
+def main():
+    print("This is 1st module and module name {}".format(__name__))
+
+if __name__ == "__main__":
+    main()
+
+

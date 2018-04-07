f156c7207f19d87614b5787ea1b2e3ebc01d09ba
krishnendu <krishnendu1985@gmail.com>
2018-04-07 21:56:04 +0530

    Worked on module, package etc and created multiple file

--- Program_Practice/count_vowels.py
+++ Program_Practice/count_vowels.py
@@ -3,16 +3,17 @@
 vowels=["a","e","i","o","u","A","E","I","O","U"]
 vowel_counts=0
 input_string=input("Enter the string to count the vowels\t")
-count={}
+count=[]
 for i in input_string:
     if i in vowels:
         vowel_counts = vowel_counts+1
+        count.append(i)
     else:
         pass
 print("Number of vowel counts is {}".format(vowel_counts))
 
 
-a={x : input_string.count(x) for x in input_string}
+a={x : input_string.count(x) for x in count}
 
 
 print(a)

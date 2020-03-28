e29337b2eacb55c5645f00434f49d217b11747fa
krishnendu kayal <krishnendu1985@gmail.com>
2020-03-28 10:03:51 +0530

    added file

--- Program_Practice_1/test.py
+++ Program_Practice_1/test.py
@@ -1,8 +1,8 @@
 print("This is my 1st program")
 print("\nPlease use of this program")
 print("Print number based on user input")
-i_n = input("Please enter your number to print\t")
-for i in range(int(i_n)):
-    print("Iteration number\t" + i)
-    new_value = i_n*i_n
-    print("Multiplication of iteration number\t" + new_value)
+i_n = int(input("Please enter your number to print\t"))
+for i in range(i_n):
+    print("Iteration number\t" + str(i))
+    new_value = int(i)*int(i)
+    print("Multiplication of iteration number\t" + str(new_value))

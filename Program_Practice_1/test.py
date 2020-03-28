ecd4039282f1e01a732f0ca0deedc59baf8494f4
krishnendu kayal <krishnendu1985@gmail.com>
2020-03-28 09:51:33 +0530

    edit line

--- Program_Practice_1/test.py
+++ Program_Practice_1/test.py
@@ -3,4 +3,6 @@ print("\nPlease use of this program")
 print("Print number based on user input")
 i_n = input("Please enter your number to print\t")
 for i in range(int(i_n)):
-    print(i)
+    print("Iteration number\t" + i)
+    new_value = i_n*i_n
+    print("Multiplication of iteration number\t" + new_value)

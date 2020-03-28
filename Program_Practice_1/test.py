e8bcb011bfa7e5e90f0d37bf77c32f27a61e8842
krishnendu kayal <krishnendu1985@gmail.com>
2020-03-28 09:46:09 +0530

    edit line in test.py

--- Program_Practice_1/test.py
+++ Program_Practice_1/test.py
@@ -1,4 +1,5 @@
 print("This is my 1st program")
+print("\nPlease use of this program")
 print("Print number based on user input")
 i_n = input("Please enter your number to print\t")
 for i in range(int(i_n)):

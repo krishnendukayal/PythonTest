5dac3f5f2ff4a6501507284c3a0395901138ff4b
krishnendu kayal <krishnendu1985@gmail.com>
2020-03-28 10:09:01 +0530

    Edit the test.py file . deleted unnecessery line

--- Program_Practice_1/test.py
+++ Program_Practice_1/test.py
@@ -1,6 +1,4 @@
-print("This is my 1st program")
-print("\nPlease use of this program")
-print("Print number based on user input")
+print("This program will take the input number and will print the number and multiplication of the number")
 i_n = int(input("Please enter your number to print\t"))
 for i in range(i_n):
     print("Iteration number\t" + str(i))

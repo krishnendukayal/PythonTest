764b7d5fae3ecc9572534f39937128bb7ecbc363
krishnendu kayal <krishnendu1985@gmail.com>
2023-12-03 13:37:47 +0530

    Update ayushman.py

--- Program_Practice/ayushman.py
+++ Program_Practice/ayushman.py
@@ -2,3 +2,19 @@
 # Author: Krishnendu Kayal
 # Email: krishnendu1985@gmail.com
 print("This is my new program")
+"""
+This s test 
+"""
+'''
+Thi is multi comment 
+'''
+# This is .format() function
+print("this is me {a} {b} {c}".format(a="and", b="my", c="test"))
+print("this is me {} {} {}".format("and", "my", "test"))
+print("this is me {1} {0} {2}".format("and", "my", "test"))
+
+#This is f function
+a="and" 
+b="my"
+c="test"
+print(f"this is me {a} {b} {c}")
